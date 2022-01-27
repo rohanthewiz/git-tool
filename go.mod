87eb2888fd8b740993a9fae5b3d312a5084dc829
Rohan Allison <rohan.allison@cbre.com>
2022-01-27 02:41:56 -0600

    Explore Table and binding

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.17
 
 require (
 	fyne.io/fyne/v2 v2.1.0
+	github.com/brianvoe/gofakeit/v6 v6.14.3
+	github.com/micheam/fyne-examples v0.0.0-20210903034107-d495b1d82cbc
 	github.com/rohanthewiz/rerr v0.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
 )
---
+++ table_exp/main.go
@@ -0,0 +1,118 @@
+package main
+
+// Code by Rohan Allison
+import (
+	"fmt"
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
+	"github.com/rohanthewiz/rerr"
+)
+
+type Animal struct {
+	Name, Type, Color, Weight string
+}
+
+const nbrAnimalAttrs = 4
+
+var animals = []Animal{
+	{Name: "Frisky", Type: "cat", Color: "gray", Weight: "10"},
+	{Name: "Ella", Type: "dog", Color: "brown", Weight: "50"},
+	{Name: "Mickey", Type: "mouse", Color: "black", Weight: "1"},
+}
+var headers = []string{"Name", "Type", "Color", "Weight"}
+
+func main() {
+	var bindings []binding.DataMap
+	for i := 0; i < len(animals); i++ {
+		bindings = append(bindings, binding.BindStruct(&animals[i]))
+	}
+
+	ap := app.New()
+	wn := ap.NewWindow("Table Widget")
+
+	tbl := widget.NewTable(
+		// dimensions
+		func() (int, int) {
+			return len(animals) + 1, nbrAnimalAttrs
+		},
+		// default
+		func() fyne.CanvasObject {
+			return widget.NewLabel(" - ")
+
+		},
+		// bindings
+		func(cell widget.TableCellID, co fyne.CanvasObject) {
+			// no binding --> co.(*widget.Label).SetText(data[i.Row][i.Col])
+			if cell.Row == 0 { // Header Row
+				label := co.(*widget.Label)
+				label.Alignment = fyne.TextAlignCenter
+				label.TextStyle = fyne.TextStyle{Bold: true}
+				// label.Text =
+				label.SetText(headers[cell.Col])
+				return
+			}
+
+			datum, err := getTableDatum(cell, bindings)
+			if err != nil {
+				return
+			}
+			co.(*widget.Label).Bind(datum.(binding.String))
+		})
+
+	refWidth := widget.NewLabel("reasonable width").MinSize().Width
+
+	tbl.SetColumnWidth(0, refWidth)
+	tbl.SetColumnWidth(1, refWidth*2/3)
+	tbl.SetColumnWidth(2, refWidth)
+	tbl.SetColumnWidth(3, refWidth*2/3)
+
+	// Handler
+	tbl.OnSelected = func(cell widget.TableCellID) {
+		if cell.Row == 0 && cell.Col < len(headers) {
+			fmt.Println("-->", headers[cell.Col])
+			return
+		}
+
+		datum, err := getTableDatum(cell, bindings)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		str, er := datum.(binding.String).Get()
+		if er != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println("-->", str)
+	}
+
+	// Layout
+	wn.SetContent(container.NewMax(tbl))
+	wn.Resize(fyne.Size{Width: 500, Height: 450})
+	wn.ShowAndRun()
+}
+
+func getTableDatum(cell widget.TableCellID, bindings []binding.DataMap,
+) (datum binding.DataItem, err error) {
+	row := bindings[cell.Row-1] // first row is header
+	switch cell.Col {
+	case 0:
+		datum, err = row.GetItem("Name")
+	case 1:
+		datum, err = row.GetItem("Type")
+	case 2:
+		datum, err = row.GetItem("Color")
+	case 3:
+		datum, err = row.GetItem("Weight")
+	}
+	if err != nil {
+		log.Println(rerr.StringFromErr(rerr.Wrap(err)))
+		return datum, rerr.Wrap(err)
+	}
+	return
+}
